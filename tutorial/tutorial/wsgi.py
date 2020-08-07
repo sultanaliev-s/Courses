eeea331de9932650fd49733d9d5f3e7d3d73c6ce
pawtny <sultanaliev_s@auca.kg>
2020-08-07 23:21:10 +0600

    updated settings

--- tutorial/tutorial/wsgi.py
+++ tutorial/tutorial/wsgi.py
@@ -1,12 +1,12 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
 
 application = get_wsgi_application()
-
-
-from whitenoise.django import DjangoWhiteNoise
 
 application = DjangoWhiteNoise(application)
