57d8c0f3c881d242c3876159cdd63fd340b59d02
pawtny <sultanaliev_s@auca.kg>
2020-08-08 00:39:45 +0600

    test

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -1,5 +1,6 @@
 import dj_database_url
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,5 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
