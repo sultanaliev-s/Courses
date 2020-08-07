766ea6e9ccc252640f7d9f435d6a7ba59fdfabc5
pawtny <sultanaliev_s@auca.kg>
2020-08-07 22:53:59 +0600

    gunicorn and postgres

--- tutorial/tutorial/wsgi.py
+++ tutorial/tutorial/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for tutorial project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
 
 application = get_wsgi_application()
+
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
